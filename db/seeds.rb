71c564c32fc634d1f0a7e1ca4576b7daa81b3b22
shinoburc <dandydot@gmail.com>
2014-02-20 08:08:50 +0900

    add seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,5 +7,21 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 # coding: utf-8
 
-User.create(:name => 'admin', :email => 'admin@local.host', :password => 'password')
+admin = User.create(name: 'admin', email: 'admin@local.host', password: 'password')
 
+event = Event.create(name: 'introduction', description: 'an introduction to CTF_portal', start_datetime: '2014/02/20 00:00:00', end_datetime: '2698/03/02 00:00:00')
+
+EventUser.create(user: admin, event: event)
+
+forensics = Genre.create(name: 'forensics')
+programming_crypt = Genre.create(name: 'programming, crypt')
+binary = Genre.create(name: 'binary')
+network_web = Genre.create(name: 'network, web')
+etc = Genre.create(name: 'etc')
+
+q1 = Question.create(name: 'echo', description: 'say "Hello World"', answer: 'Hello World', genre: etc, default_point: 100)
+q2 = Question.create(name: 'expr', description: '1 + 2', answer: '3', genre: etc, default_point: 100)
+
+event.questions.push q1
+event.questions.push q2
+event.save
