0cf5db4fe3dce992e3d86c429c8af12bc41409ea
shinoburc <dandydot@gmail.com>
2014-02-24 02:00:34 +0900

    add dependent destroy  event_user for cleared_user_quections.

--- app/models/event_user.rb
+++ app/models/event_user.rb
@@ -2,7 +2,7 @@ class EventUser < ActiveRecord::Base
   belongs_to :event
   belongs_to :user
 
-  has_many :cleared_user_questions
+  has_many :cleared_user_questions, dependent: :destroy
   has_many :event_questions, through: :cleared_user_questions
 
   validates :event_id,:uniqueness => {:scope =>:user_id}
