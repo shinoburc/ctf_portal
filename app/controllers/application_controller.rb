6e22b90a4380cf5ee0e48cf4b6d9841e3a22485d
shinoburc <dandydot@gmail.com>
2014-02-23 23:48:25 +0900

    Add devise registration views for name columnd.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :update_sanitized_params, if: :devise_controller?
   before_action :authenticate_user!
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
@@ -34,5 +35,8 @@ class ApplicationController < ActionController::Base
       if !ApplicationController.helpers.event_is_ready?(@event, current_user)
         redirect_to root_path
       end
+    end
+    def update_sanitized_params
+      devise_parameter_sanitizer.for(:sign_up) {|u| u.permit(:name, :email,   :password, :password_confirmation)}
     end
 end
