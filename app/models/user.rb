95309c1231efa3ddd941181e45cd902ad4b53c03
shinoburc <dandydot@gmail.com>
2014-02-24 01:46:34 +0900

    Add validation for devise user name.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,11 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
   has_many :event_users
   has_many :events, through: :event_users
   has_many :group_users
   has_many :groups, through: :group_users
+
+  validates_presence_of :name
 end
